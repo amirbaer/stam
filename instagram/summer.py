8d220b19c8894f1c442542420e060a204af94e9c
HSamirbaer <amir@hiredscore.com>
2015-01-27 10:00:26 +0200

    done instagram

--- instagram/summer.py
+++ instagram/summer.py
@@ -6,7 +6,7 @@ import os
 import pickle
 import glob
 
-def main(pickles_folder, limit):
+def main(pickles_folder, limit, until = None):
     res = {}
 
     for pickle_file in glob.glob(os.path.join(pickles_folder, "*.pickle")):
@@ -15,18 +15,39 @@ def main(pickles_folder, limit):
         res.update(cur)
 
     indexes = sorted(res.keys())
-    print "found a total of %s items (first: %s, last: %s)" % (len(indexes), indexes[0], indexes[-1])
+    first = indexes[0]
+    last = indexes[-1]
+    print "found a total of %s items (first: %s, last: %s)" % (len(indexes), first, last)
     indexes = set(indexes)
-    expected = set(xrange(indexes[-1] + 1))
-    diff = expected - indexes
-    print "missing %d items: %s" % (len(diff), ",".join(diff))
+    expected = set(xrange(last + 1))
+    diff = list(expected - indexes)
+    #TODO: convert to ranges
+
+    print "missing %d items" % len(diff),
+    items = []
+    if len(diff) > 0:
+        start = diff[0]
+        prev = diff[0]
+        for index in diff[1:]:
+            if index - prev != 1: 
+                items.append((start, prev)) # inclusive
+                start = index
+            prev = index
+        items.append((start, prev)) # inclusive
+        items = sorted(items)
+
+        print ":\n%s" % ",".join([start == finish and str(start) or "%d - %d" % (start, finish) for start, finish in items])
+
+    print
+    print "Results:"
 
     y = {}
     for i in res.values():
         y[int(i['data']['name'])] = i['data']['media_count']
 
     for i,v in y.items():
-        if v < limit:
+        if until != None and i <= until:
+            if v <= limit and i <= until:
                 print i,v
 
 
@@ -34,15 +55,19 @@ def main(pickles_folder, limit):
 if __name__ == "__main__":
 
     if len(sys.argv) < 3:
-        print "usage: %s <pickles_folder> <limit>" % sys.argv[0]
+        print "usage: %s <pickles_folder> <limit> [<until>]" % sys.argv[0]
         sys.exit(1)
 
-    pickles_folder = args[0]
-    limit = int(args[1])
+    pickles_folder = sys.argv[1]
+    limit = int(sys.argv[2])
+    until = None
+
+    if len(sys.argv) == 4:
+        until = int(sys.argv[3])
 
     if not os.path.isdir(pickles_folder):
         print "%s is not a folder or doesn't exist" % pickles_folder
         sys.exit(1)
 
-    main(pickles_folder, limit)
+    main(pickles_folder, limit, until)
 
